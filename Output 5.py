e7429b5255c1ea47df142d2dfe511350c81b3a08
ManishSharma1609 <104347990+ManishSharma1609@users.noreply.github.com>
2022-06-20 23:25:50 +0530

    Add files via upload

---
+++ Output 5.py
@@ -0,0 +1,158 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: D:/Manish/New folder (2)/Output 5.py ================
+Enter a string :Hello World
+Reversed string is dlroW olleH 
+
+Enter the range of numbers: 1 50
+Enter number: 3
+3
+6
+9
+12
+15
+18
+21
+24
+27
+30
+33
+36
+39
+42
+45
+48
+Enter first side of the triangle: 10
+Enter second side of the triangle: 11
+Enter third side of the triangle: 12
+Area of the triangle is 51.521233486786784 
+
+* 
+
+* * 
+
+* * * 
+
+* * * * 
+
+* * * * * 
+
+* * * * 
+
+* * * 
+
+* * 
+
+* 
+
+Enter number of rows : 4
+A
+
+BC
+
+DEF
+
+GHIJ
+
+Enter the range of numbers: 1 100
+2 
+
+3 
+
+5 
+
+7 
+
+11 
+
+13 
+
+17 
+
+19 
+
+23 
+
+29 
+
+31 
+
+37 
+
+41 
+
+43 
+
+47 
+
+53 
+
+59 
+
+61 
+
+67 
+
+71 
+
+73 
+
+79 
+
+83 
+
+89 
+
+97 
+
+77 
+
+154 
+
+231 
+
+308 
+
+385 
+
+462 
+
+Enter 1 number: 1
+Enter 2 number: 55
+Enter 3 number: -99
+Enter 4 number: -8
+Enter 5 number: 55
+Enter 6 number: 0
+Enter 7 number: -76
+Enter 8 number: 13
+Enter 9 number: 98
+Enter 10 number: -22
+[1, 55, -99, -8, 55, 0, -76, 13, 98, -22]
+Positive numbers are: 
+1
+55
+55
+13
+98
+Negative numbers are: 
+-99
+-8
+-76
+-22
+Odd numbers are: 
+1
+55
+-99
+55
+13
+Even numbers are: 
+-8
+0
+-76
+98
+-22
+No of times each number occurs in the List:  {1: 1, 55: 2, -99: 1, -8: 1, 0: 1, -76: 1, 13: 1, 98: 1, -22: 1} 
+
+Enter a string: program will count the number of occurrences of each word in the list
+{'program': 1, 'will': 1, 'count': 1, 'the': 2, 'number': 1, 'of': 2, 'occurrences': 1, 'each': 1, 'word': 1, 'in': 1, 'list': 1}
